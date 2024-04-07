60d087b9f33413abe7fb9ebb2cdc4a78f46e98e1
AzmeenaAbdulJabbbar <56413566+AzmeenaAbdulJabbbar@users.noreply.github.com>
2024-04-08 04:21:53 +0500

    guessing-game

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber =Math.floor(Math.random()*10) ; 
+//console.log (randomNumber)
+const numbers = await inquirer.prompt([
+    {
+        name : "userGuessNumber",
+        type:"number",
+        message: "please guess the number between 1-10",
+    }
+])
+if (numbers.userGuessNumber === randomNumber){
+    console.log("congratulations!")
+}
+else{
+    console.log( "please try again")
+}
+console.log(numbers);
+//the end//
